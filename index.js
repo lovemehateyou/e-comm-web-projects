dc4f8e28d2b7f47138d3e9c62554ada3ddc11491
lovemehateyou <alazarzerubabel6@gmail.com>
2024-09-13 19:34:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,11 +17,20 @@ const corsOptions = {
     origin: ['https://main--habeshshops.netlify.app'], // Allow your Netlify frontend
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods 
     allowedHeaders: ['Content-Type', 'Authorization'],
-    
+
     credentials: true // Allow credentials (cookies, etc.)
 };
 
 app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://main--habeshshops.netlify.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
 app.use(bodyParser.json());
 app.use(cookieParser());
 
