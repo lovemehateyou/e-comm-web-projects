91af5eb971fe0d83b5a5ada834e29a8e7107baa3
lovemehateyou <alazarzerubabel6@gmail.com>
2024-09-27 16:21:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const User = mongoose.model('User', userSchema);
 
 // Middleware to parse incoming JSON requests and cookies
 const corsOptions = {
-    origin: ['https://main--habeshshops.netlify.app', 'http://localhost:5500','http://127.0.0.1:5500'],
+    origin: ['https://main--habeshshops.netlify.app', 'http://localhost:5500','http://127.0.0.1:5500','https://habeshshops.netlify.app'],
 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods 
     allowedHeaders: ['Content-Type', 'Authorization'],
